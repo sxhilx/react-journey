4ca2a2de495e4aca562d5a29321698a7c7bd4cb4
sxhilx <jadasahil786@gmail.com>
2024-11-03 14:18:18 +0200

    Learned about hooks and tried useState by creating a simple counter project

---
+++ counter-02/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(5) // the useState hook, changes the value wherever its being used.
+  let [msg, setMsg] = useState("")
+
+
+  const addValue = () => {
+    if(counter < 10){
+      setCounter(counter + 1);  
+      setMsg("")
+    }else{
+      setMsg("Maximum value reached")
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1);  
+    }else{
+      setMsg("Minimum value reached")
+    }
+  }
+
+
+  return (
+    <>
+      <h1>Sammy learning react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue} >Add Value {counter}</button>
+      <br /><br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+
+      {msg && <p>{msg}</p>}
+     
+    </>
+  )
+}
+
+export default App
