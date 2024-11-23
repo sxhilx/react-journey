1510e8eda3283d1f1ba020d33e06dc2ac7a24d1e
sxhilx <jadasahil786@gmail.com>
2024-11-23 17:44:58 +0200

    imporvements

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -15,9 +15,9 @@ const InputBox = ({
     <div className='flex max-w-lg mx-auto bg-slate-100 shadow-lg p-3 justify-between rounded'>
         <div className='flex flex-col w-1/2'>
             <label htmlFor="amount" className='mb-4 text-gray-600 px-1'> {label} </label>
-            <input type="number" id="amount" className='outline-none bg-slate-100 px-1 text-2xl text-gray-500 appearance-none' placeholder='amount'
-            value={amount}
-            onChange={(event) => {
+            <input type="number" id="amount" className='outline-none bg-slate-100 px-1 text-2xl text-gray-500 appearance-none' placeholder='0'
+            value={amount || ""}      
+             onChange={(event) => {
                 const value = Number(event.target.value); 
                 onAmountChange && onAmountChange(value); 
             }}/>
