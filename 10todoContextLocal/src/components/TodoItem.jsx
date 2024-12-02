9dfe4bb3a095d1ed6defd2dea2f06a134c53c85a
sxhilx <jadasahil786@gmail.com>
2024-12-02 22:43:01 +0200

    updates

--- 10todoContextLocal/src/components/TodoItem.jsx
+++ 10todoContextLocal/src/components/TodoItem.jsx
@@ -20,7 +20,8 @@ function TodoItem({ todo }) {
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
                 todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
-        >
+        >   
+            
             <input
                 type="checkbox"
                 className="cursor-pointer"
