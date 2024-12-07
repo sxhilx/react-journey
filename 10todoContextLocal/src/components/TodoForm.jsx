1ad9b04c07e490bd0762189cb6bf358b8576c0b0
sxhilx <jadasahil786@gmail.com>
2024-12-07 14:57:21 +0200

    updates

--- 10todoContextLocal/src/components/TodoForm.jsx
+++ 10todoContextLocal/src/components/TodoForm.jsx
@@ -20,7 +20,7 @@ function TodoForm() {
             <input
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                className="w-full border border-black/10 rounded-l-lg px-4 outline-none duration-150 bg-white/20 py-1.5"
                 value={todo}
                 onChange={(e) => setTodo(e.target.value)}
             />
